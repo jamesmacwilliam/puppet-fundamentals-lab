3241eec71f0c0b47a9755c1b5ad3194529759018
James Mac William <jimmy.macwilliam@gmail.com>
2018-08-14 23:35:28 -0400

    remove unused cookbooks

--- cookbooks/puppetmaster/metadata.rb
+++
@@ -1,8 +0,0 @@
-name 'puppetmaster'
-maintainer 'James Mac William'
-maintainer_email 'jimmy.macwilliam@gmail.com'
-license 'All Rights Reserved'
-description 'Installs/Configures puppet'
-long_description 'Installs/Configures puppet'
-version '0.1.0'
-chef_version '>= 12.14' if respond_to?(:chef_version)
--- cookbooks/puppetmaster/recipes/default.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook:: puppetmaster
-# Recipe:: default
-#
-# Copyright:: 2018, James Mac William, All Rights Reserved.
-
-include_recipe 'puppetmaster::firewall'
-include_recipe 'puppetmaster::install'
-include_recipe 'puppetmaster::environments' if node['puppetmaster']['environments_git_repo']
